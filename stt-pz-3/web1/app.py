724436ed42216d2d95eda02f7f3f33ac4aa8bd1f
Horov4ik <horov4ik@gmail.com>
2026-03-04 15:19:13 +0200

    2.0

--- stt-pz-3/web1/app.py
+++ stt-pz-3/web1/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.get('/')
 def index():
-    return jsonify({"service": "web1", "status": "ok"})
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
